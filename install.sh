358d88620b86d8acfe057004af9211ace9d58ec7
Netflixxp <61532011+Netflixxp@users.noreply.github.com>
2021-12-01 19:25:35 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,23 +7,23 @@ echo -e "频道 https://t.me/mffjc\n"
 
 localIP=$(ip -o -4 addr list | grep -Ev '\s(docker|lo)' | awk '{print $4}' | cut -d/ -f1 | grep -o "10.[0-9]\{1,3\}\.[0-9]\{1,3\}\.")
 
-wget -O /usr/local/bin/netflix.sh https://raw.githubusercontent.com/yxkumad/streammonkeylite/main/zoe_hinet.sh>/dev/null 2>&1 && chmod +x /usr/local/bin/netflix.sh
+wget -O /usr/local/bin/netflix.sh https://raw.githubusercontent.com/Netflixxp/changip/main/netflix.sh>/dev/null 2>&1 && chmod +x /usr/local/bin/netflix.sh
 
 echo -e "请输入服务器名称"
 read -erp "(默认: jcnf):" name
 [[ -z "$name" ]] && zoecloudname="jcnf"
 
-echo -e "请输入 Telegram Bot Token"
+echo -e "请输入 Telegram Bot Token 前往TG@Botfather"
 read -erp "(默认: null):" bottoken
 [[ -z "$bottoken" ]] && bottoken="null"
 
-echo -e "请输入 Telegram Chat ID"
+echo -e "请输入 Telegram Chat ID 前往TG@userinfobot"
 read -erp "(默认: null):" botchatid
 [[ -z "$botchatid" ]] && botchatid="null"
 
 echo -e "请输入检测频率"
-read -erp "(默认: 5):" crontime
-[[ -z "$crontime" ]] && crontime="5"
+read -erp "(默认: 10):" crontime
+[[ -z "$crontime" ]] && crontime="10"
 
 echo -e "请输入换IP的api"
 read -erp "(默认: null):" bottoken
